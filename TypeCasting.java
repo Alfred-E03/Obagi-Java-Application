c0f77f16934b6e176a229246780c1f9d3aef723a
Alfred-E03 <alfredenyinna03@gmail.com>
2025-03-24 11:19:13 +0100

    Word Document Hometask

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+public class TypeCasting{
+	public static void main(String [] args){
+	//widening type cast
+	double weight = 675;
+	System.out.printf("Your wieght is %f %n",weight);
+	
+	//Narrowing type casting ? =63
+	char alpha = '?' ;
+	
+	int asciiCode = (char)alpha;
+	System.out.printf("The ASCII code for ? is %d",asciiCode);
+	
+	double height = 5.8;
+	int myHeight= (int)height;
+	System.out.printf("My faculty's height is %d %n", myHeight);
+	}
+}
