78f752ff5ac1bd66133fad4b113252ee4234fae1
Alfred-E03 <alfredenyinna03@gmail.com>
2025-04-02 14:12:52 +0100

    Chapter 3.docx

---
+++ ForLoopStatement.java
@@ -0,0 +1,18 @@
+public class ForLoopStatement{
+	public static void main(String[] args){
+		
+		int num = 50;
+		String name = "Alfred";
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%d%n",num);
+		}
+		
+		for (int j = 1; j <=10; j++){
+			 System.out.printf("%s%n",name);
+		 }
+	}
+	
+	}
+		
+		 
